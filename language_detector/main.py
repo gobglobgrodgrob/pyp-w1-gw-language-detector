95799b811da6bc8f1aa3789db924e3629fcac5a2
Nicholas Chow <nick.chow.zj@gmail.com>
2016-07-09 08:38:41 +0000

    Our second solution!

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,10 +5,21 @@ from .languages import LANGUAGES
 
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
+    counter = {}
     for diction in languages:
+        counter[diction['name']] = 0
         for word in diction['common_words']:
             for text_word in text.split():
                 if word == text_word:
-                    return diction['name']
+                    counter[diction['name']] += 1
     
-    #return diction['name'] if word == text_word (for diction in languages) (for word in diction['common_words']) (for text_word in text.split()) 
+    return max(counter, key = counter.get)
+    
+    # return [k for k, v in counter.items() if v == max(counter.values())][0]
+    # pre-converted list comp 
+    # for k, v in counter.items():
+    #     if v == max(counter.values()):
+    #         return k
+            
+    # attempted list comp before adding counter         
+    # return diction['name'] for diction in languages for word in diction['common_words'] for text_word in text.split() if word == text_word
